af48d0fa6336ebc91910ea0c810ae54f3c56fc89
BogacSabuncu <bsabuncu@mac.com>
2019-05-22 11:33:29 -0400

    Added comments to the code

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -4,18 +4,23 @@ let friendList = require("../data/friends");
 //export the routing
 module.exports = function(app){
 
+    //api get request for all the friends
     app.get("/api/allFriends", function(req, res){
         res.json(friendList);
     });
 
+    //api post request for all the friends
     app.post("/api/allFriends", function(req, res){
         friendList.push(req.body);
         res.json(true); 
     });
 
+    //to get the best friend result
     app.post("/api/bestfriend", function(req, res){
+        //call the best friend funtion that returns the best match.
         let bestfriend = require("../../findFriend")(req.body);
 
+        //return bestfriend as JSON
        res.json(bestfriend);
     });
 };
--- findFriend.js
+++ findFriend.js
@@ -1,23 +1,31 @@
 let friendList = require("./app/data/friends");
 
+//function for finding the bestfriend
 module.exports = function(newFriend){
 
     let bestFriend = {};
     let bestFriendDif = 100;
 
+    //for each friend in the datafile
     friendList.forEach(friend => {
+        
         let diffrence = 0;
+        //go through their individual answers
         for(let i = 0; i < 10; i++){
+            //if they are not equal to each other
             if(friend.scores[i] != newFriend.scores[i]){
+                //get the difference
                diffrence += Math.abs(parseInt(friend.scores[i])-parseInt(newFriend.scores[i]));
             }
         }
-        //console.log(diffrence + " | " + bestFriendDif )
+        //the friend with the least difference becomes the new best friend
         if(diffrence <= bestFriendDif){
             bestFriendDif = diffrence;
             bestFriend = friend;
         }
         
     });
+
+    //return the bestfriend
     return bestFriend;
 }
